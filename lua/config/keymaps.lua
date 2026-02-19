e68e1a608a905ea8f4e84e46a78681c3e8cbc886
Joslef <43706650+Joslef@users.noreply.github.com>
2026-02-19 09:56:02 +0100

    Add new keymaps for OpenCode functionality

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,58 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>h", function()
-  Snacks.dashboard.open()
-end, { desc = "Dashboard" })
+local wk = require("which-key")
+
+wk.add({
+  { "<leader>o", group = "OpenCode" },
+  {
+    "<leader>oa",
+    function()
+      require("opencode").ask("@this: ", { submit = true })
+    end,
+    desc = "Ask about this",
+  },
+  {
+    "<leader>os",
+    function()
+      require("opencode").select()
+    end,
+    desc = "Select prompt",
+  },
+  {
+    "<leader>o+",
+    function()
+      require("opencode").prompt("@this")
+    end,
+    desc = "Add this",
+  },
+  {
+    "<leader>ot",
+    function()
+      require("opencode").toggle()
+    end,
+    desc = "Toggle embedded",
+  },
+  {
+    "<leader>oc",
+    function()
+      require("opencode").command()
+    end,
+    desc = "Select command",
+  },
+  {
+    "<leader>oA",
+    function()
+      require("opencode").command("agent_cycle")
+    end,
+    desc = "Cycle selected agent",
+  },
+  {
+    "<leader>h",
+    function()
+      Snacks.dashboard.open()
+    end,
+    desc = "Dashboard",
+    icon = "",
+  },
+})
